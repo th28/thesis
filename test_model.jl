d31be15bae5410ecc385a5523318acbe86169630
Tom H <7756136+th28@users.noreply.github.com>
2022-08-29 20:09:58 +0300

    contract constraints daca

--- test_model.jl
+++ test_model.jl
@@ -6,6 +6,7 @@ using GLPK
 using HiGHS
 using DataFrames
 using XLSX
+using DisjunctiveProgramming
 
 include("utility.jl")
 
@@ -27,6 +28,8 @@ for sheet in sheet_names
 end
 
 # Parameters
+dacalim = df2param(data["DACA_limits"])
+dacapr = df2param(data["DACA_prices"])
 a = df2param(data["RawMaterialConversion"])
 RP = df2param(data["RawMaterialPrices"])
 D = df2param(data["CustomerDemand"])
@@ -37,11 +40,10 @@ L = df2param(data["LogisticCosts"])
 SC = df2param(data["StorageCosts"])
 ST = df2param(data["StorageCapacities"])
 PR = df2param(data["Prices"])
-# WC = df2param(data["RawMaterialCosts"])
 EC = df2param(data["EnergyCosts"])
 
 # Sets 
-
+A = unique(data["Contracts"].CONTRACT)
 M = unique(data["PM"].MILL)
 PM = unique(data["PM"].PM)
 P = unique(data["CustomerDemand"].PRODUCT)
@@ -71,18 +73,28 @@ mod = Model(GLPK.Optimizer)
 # Decision Variables
 
 # Integer decision variables
+@variable(mod, z[A, R, T], Bin)
 @variable(mod, y[PM], Bin)
 
+
 # Cont. decision variables
+@variable(mod, RC[A,R,T] >= 0)
+# DACA contract ad-hoc variables
+@variable(mod, daca[["d1","d2"],R, T] >= 0)  #contract stage, raw mater. time period
+
+
+
 @variable(mod, RB[T,R] >= 0) # raw material purchased at time T
 @variable(mod, RI[T_lag_fix, R, M] >= 0) # raw material inventory
 @variable(mod, x[P, PM, T, C] >= 0)
 @variable(mod, I[P, M , T_lag_fix, C] >= 0)
 
+@expression(mod, rcost_f, sum(RP[t,r]*RC["FIXED",r, t] for r in R, t in T))
+@expression(mod, rcost_daca, sum(dacapr[t, "d1", r]*daca["d1", r, t] + dacapr[t, "d2", r]*daca["d1", r, t] for r in R, t in T))
 # Objective components (linear expressions)
 @expression(mod, sales, sum(PR[t,i]*x[i,p,t,c] for i in P, p in PM, t in T, c in C))
-#@expression(mod, pcost, sum(sum(WC[t,p,i,r] for r in prod_mat[i])*x[i,p,t,c] for i in P, p in PM, t in T, c in C))
-@expression(mod, rcost, sum(RP[t,r]*RB[t,r] for t in T, r in R))
+#@expression(mod, rcost, sum(RP[t,r]*RB[t,r] for t in T, r in R))
+@expression(mod, rcost, rcost_f + rcost_daca)
 @expression(mod, icost, sum(SC[m]*I[i,m,t,c] for m in M, i in P, t in T, c in C) + sum(SC[m]*RI[t,r,m] for t in T, r in R, m in M))
 @expression(mod, lcost, sum(L[c,m]*x[i,p,t,c] for i in P, p in PM, t in T, c in C, m in M))
 @expression(mod, ecost, sum(sum(EC[t,pm_mill[p],i,r] for r in prod_en[i])*x[i,p,t,c] for i in P, p in PM, t in T, c in C))
@@ -92,6 +104,20 @@ mod = Model(GLPK.Optimizer)
 @objective(mod, Max, sales - rcost - icost - ecost - lcost - onoff)
 
 # Constraints
+#Contract Constraints
+#Only one contract allowed
+@constraint(mod, [t in T, r in R], sum(z[a, r, t] for a in A) <= 1)
+#Only exercise active Contracts
+@constraint(mod, [a in A, r in R, t in T], RC[a, r, t] <= 100000000*z[a, r, t] )
+#Aggregate raw material purchases
+@constraint(mod, [t in T, r in R], RB[t,r] == sum(RC[a, r, t] for a in A))
+#DACA
+@constraint(mod, [r in R, t in T], RC["DACA", r, t] == daca["d1", r, t] + daca["d2", r, t])
+
+
+
+
+
 @constraint(mod, [i in P, m in M,c in C], I[i, m, 202200, c] == 0)
 @constraint(mod, [r in R, m in M], RI[202200, r, m] == 0)
 
